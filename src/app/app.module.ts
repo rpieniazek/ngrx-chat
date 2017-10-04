ac6ca75fee4eb874b998c73993b78ea1c8442070
rafalpieniazek <rkpieniazek@gmail.com>
2017-10-04 19:37:48 +0200

    components added, project bootstraped

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MessageListComponent } from './message-list/message-list.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
+import { ThreadSectionComponent } from './thread-section/thread-section.component';
+import { UserSectionComponent } from './user-section/user-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageListComponent,
+    MessageSectionComponent,
+    ThreadListComponent,
+    ThreadSectionComponent,
+    UserSectionComponent
   ],
   imports: [
     BrowserModule
